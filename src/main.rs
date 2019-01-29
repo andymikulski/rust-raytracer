d0c7c8088be3e2e34226b2c0ec5c36c4f8311e60
Andy Mikulski <andy.mikulski@gmail.com>
2019-01-29 01:04:42 -0500

    add hitable list and cool lifetime stuff, wrong colors though

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ rust questions:
 */
 #![allow(dead_code)]
 extern crate image;
+extern crate time;
 
 use std::ops;
 
@@ -47,13 +48,12 @@ impl Vec3 {
         return Vec3::new(self.x * k, self.y * k, self.z * k);
     }
 
-    pub fn to_unit_vector(mut self) -> Vec3 {
-        self = self.get_unit_vector();
-        return self;
-    }
-
     pub fn dot(one: &Vec3, two: &Vec3) -> f32 {
         return (one.x * two.x) + (one.y * two.y) + (one.z * two.z);
+    }
+
+    pub fn unit_vector(one: &Vec3) -> Vec3 {
+        return one.get_unit_vector();
     }
 
     pub fn cross(&self, rhs: &Vec3) -> Vec3 {
@@ -204,37 +204,128 @@ impl Ray {
     }
 }
 
+struct HitRecord {
+    t: f32,
+    p: Vec3,
+    normal: Vec3,
+}
+
+trait Hitable {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
+}
+
+struct Sphere {
+    center: Vec3,
+    radius: f32,
+}
+
+impl Sphere {
+    pub fn new(center: Vec3, radius: f32) -> Sphere {
+        return Sphere { center, radius };
+    }
+}
+
+impl Hitable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+        let oc = r.origin - self.center;
+
+        let a = Vec3::dot(&r.direction, &r.direction);
+        let b = 2.0 * Vec3::dot(&oc, &r.direction);
+        let c = Vec3::dot(&oc, &oc) - (self.radius * self.radius);
+        let discriminant = (b * b) - (4.0 * a * c);
+
+        if discriminant > 0.0 {
+            let mut temp = (-b - (b * b - a * c).sqrt()) / a;
+            if temp > t_min && temp < t_max {
+                rec.t = temp;
+                rec.p = r.point_at_parameter(temp);
+                rec.normal = (rec.p - self.center) / self.radius;
+                return true;
+            }
+
+            temp = (-b + (b * b - a * c).sqrt()) / a;
+            if temp > t_min && temp < t_max {
+                rec.t = temp;
+                rec.p = r.point_at_parameter(temp);
+                rec.normal = (rec.p - self.center) / self.radius;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+struct HitableList<'a> {
+    list: Vec<&'a Hitable>,
+}
+
+impl<'a> HitableList<'a> {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord {
+            t: 0.0,
+            p: Vec3::new(0.0, 0.0, 0.0),
+            normal: Vec3::new(0.0, 0.0, 0.0),
+        };
+
+        let mut hit_anything = false;
+        let mut closest_yet = t_max;
+
+        for hitter in &self.list {
+            let has_hit = hitter.hit(r, t_min, t_max, &mut temp_rec);
+            if has_hit {
+                hit_anything = true;
+                if closest_yet > temp_rec.t {
+                    closest_yet = temp_rec.t;
+                    rec.normal = temp_rec.normal;
+                    rec.p = temp_rec.p;
+                    rec.t = temp_rec.t;
+                }
+            }
+        }
+
+        return hit_anything;
+    }
+}
+
 //
 //
 // --- Actual program
 //
 //
 
-fn get_ray_color(r: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Vec3::new(1.0, 0.0, 0.0);
+fn get_ray_color(r: &Ray, hitables: &HitableList) -> Vec3 {
+    let mut rec = HitRecord {
+        t: 0.0,
+        p: Vec3::new(0.0, 0.0, 0.0),
+        normal: Vec3::new(0.0, 0.0, 0.0),
+    };
+
+    if hitables.hit(r, 0.0, std::f32::MAX, &mut rec) {
+        return (rec.normal + 1.0) * 0.5;
+    } else {
+        let unit_dir = r.direction.get_unit_vector();
+        let t = (unit_dir.y + 1.0) * 0.5;
+        return (Vec3::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Vec3::new(0.5, 0.7, 1.0) * t);
     }
 
-    let unit_dir = r.direction.get_unit_vector();
-    let t: f32 = 0.5 * (unit_dir.y + 1.0);
+    // let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
 
-    return (Vec3::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Vec3::new(0.5, 0.7, 1.0) * t);
-}
+    // if t > 0.0 {
+    //     let temp = r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0);
+    //     let n = Vec3::unit_vector(&temp);
+    //     return (n + 1.0) * 0.5;
+    // }
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> bool {
-    let oc = r.origin - *center;
-
-    let a = Vec3::dot(&r.direction, &r.direction);
-    let b = 2.0 * Vec3::dot(&oc, &r.direction);
-    let c = Vec3::dot(&oc, &oc) - (radius * radius);
-    let discriminant = (b * b) - (4.0 * a * c);
-
-    return discriminant > 0.0;
+    // let unit_dir = r.direction.get_unit_vector();
+    // let t = (unit_dir.y + 1.0) * 0.5;
+    // return (Vec3::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Vec3::new(0.5, 0.7, 1.0) * t);
 }
 
 fn main() {
-    let width: u32 = 200;
-    let height: u32 = 100;
+    let start = time::get_time();
+    let width: u32 = 600;
+    let height: u32 = 300;
     let mut buffer = image::ImageBuffer::new(width, height);
 
     let lower_left = Vec3::new(-2.0, -1.0, -1.0);
@@ -242,18 +333,34 @@ fn main() {
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
 
+    let mut collision_list = HitableList { list: vec![] };
+
+    let world_objs = vec![
+        Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5),
+        Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0),
+    ];
+
+    collision_list.list.push(&world_objs[0]);
+    collision_list.list.push(&world_objs[1]);
+
     for (x, y, pixel) in buffer.enumerate_pixels_mut() {
         let u: f32 = x as f32 / width as f32;
-        let v: f32 = y as f32 / height as f32;
+        let v: f32 = 1.0 - (y as f32 / height as f32);
 
         let dir = lower_left + horizontal * u + vertical * v;
         let r = Ray::new(&origin, &dir);
 
-        let mut color = get_ray_color(&r);
+        let mut color = get_ray_color(&r, &collision_list);
         color *= 255.0;
 
         *pixel = image::Rgb([color.r(), color.g(), color.b()]);
     }
+
+    let end = time::get_time();
+    let start_ms = (start.sec as i64 * 1000) + (start.nsec as i64 / 1000 / 1000);
+    let end_ms = (end.sec as i64 * 1000) + (end.nsec as i64 / 1000 / 1000);
+
+    println!("time taken: {}ms", end_ms - start_ms);
 
     // Save the image as “fractal.png”, the format is deduced from the path
     buffer.save("fractal.png").unwrap();
